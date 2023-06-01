681c3bb0381bc1417d3b078e070a56b9f0bdb14f
soetshi <90394673+soetshi@users.noreply.github.com>
2023-06-02 00:31:17 +0200

    Ajout convertion quaternions-matrices

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+
 #include <iostream>
 #include <cmath>
+#include <array>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -102,6 +104,8 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos);
 void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+Quaternion MatrixToQuaternion(const std::array<std::array<double, 3>, 3>& matrix);
+std::array<std::array<double, 3>, 3> QuaternionToMatrix(const Quaternion& q);
 void DrawCube(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength);
 
 GLfloat rotationX = 0.0f;
@@ -181,6 +185,23 @@ int main(void)
     glfwTerminate();
 
     return 0;
+
+    /*Quaternion q(0.7, 0.0, 1.0, 0.0);
+
+    std::array<std::array<double, 3>, 3> matrix = QuaternionToMatrix(q);
+    std::cout << "Matrix : " << std::endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    Quaternion convertedQuaternion = MatrixToQuaternion(matrix);
+    std::cout << "Quaternion converted : " << convertedQuaternion.getW() << " "
+              << convertedQuaternion.getX() << " " << convertedQuaternion.getY() << " "
+              << convertedQuaternion.getZ() << std::endl;*/
+
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -247,6 +268,86 @@ void scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
         zoom = 10.0f;
     }
 }
+
+std::array<std::array<double, 3>, 3> QuaternionToMatrix(const Quaternion& q) {
+    std::array<std::array<double, 3>, 3> matrix;
+
+    double w = q.getW();
+    double x = q.getX();
+    double y = q.getY();
+    double z = q.getZ();
+
+    double w2 = w * w;
+    double x2 = x * x;
+    double y2 = y * y;
+    double z2 = z * z;
+
+    matrix[0][0] = w2 + x2 - y2 - z2;
+    matrix[0][1] = 2 * (x * y - w * z);
+    matrix[0][2] = 2 * (x * z + w * y);
+
+    matrix[1][0] = 2 * (x * y + w * z);
+    matrix[1][1] = w2 - x2 + y2 - z2;
+    matrix[1][2] = 2 * (y * z - w * x);
+
+    matrix[2][0] = 2 * (x * z - w * y);
+    matrix[2][1] = 2 * (y * z + w * x);
+    matrix[2][2] = w2 - x2 - y2 + z2;
+
+    return matrix;
+}
+
+Quaternion MatrixToQuaternion(const std::array<std::array<double, 3>, 3>& matrix) {
+    double m11 = matrix[0][0];
+    double m12 = matrix[0][1];
+    double m13 = matrix[0][2];
+    double m21 = matrix[1][0];
+    double m22 = matrix[1][1];
+    double m23 = matrix[1][2];
+    double m31 = matrix[2][0];
+    double m32 = matrix[2][1];
+    double m33 = matrix[2][2];
+
+    double trace = m11 + m22 + m33;
+
+    if (trace > 0) {
+        double s = 0.5 / std::sqrt(trace + 1.0);
+        double w = 0.25 / s;
+        double x = (m32 - m23) * s;
+        double y = (m13 - m31) * s;
+        double z = (m21 - m12) * s;
+
+        return Quaternion(w, x, y, z);
+    }
+    else if (m11 > m22 && m11 > m33) {
+        double s = 2.0 * std::sqrt(1.0 + m11 - m22 - m33);
+        double w = (m32 - m23) / s;
+        double x = 0.25 * s;
+        double y = (m12 + m21) / s;
+        double z = (m13 + m31) / s;
+
+        return Quaternion(w, x, y, z);
+    }
+    else if (m22 > m33) {
+        double s = 2.0 * std::sqrt(1.0 + m22 - m11 - m33);
+        double w = (m13 - m31) / s;
+        double x = (m12 + m21) / s;
+        double y = 0.25 * s;
+        double z = (m23 + m32) / s;
+
+        return Quaternion(w, x, y, z);
+    }
+    else {
+        double s = 2.0 * std::sqrt(1.0 + m33 - m11 - m22);
+        double w = (m21 - m12) / s;
+        double x = (m13 + m31) / s;
+        double y = (m23 + m32) / s;
+        double z = 0.25 * s;
+
+        return Quaternion(w, x, y, z);
+    }
+}
+
 
 void DrawCube(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength)
 {
